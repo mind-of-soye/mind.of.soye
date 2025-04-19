28c612dde8afa26fe3b630d32b16631e9b70818e
Soye Usoroh <usorohsoye@gmail.com>
2025-04-19 17:06:08 +0100

    add heading to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import HeadingWithPageTitle from "./components/HeadingWithPageTitle";
+import Main from "./components/Main";
 import Taskbar from "./components/Taskbar";
 
 function App() {
   return (
     <>
       <Taskbar />
-      <HeadingWithPageTitle />
+      <Main />
     </>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import HeadingWithPageTitle from "./HeadingWithPageTitle";
+import "./styles/Main.css";
+
+const Main = () => {
+  return (
+    <main>
+      <HeadingWithPageTitle />
+    </main>
+  );
+};
+
+export default Main;
