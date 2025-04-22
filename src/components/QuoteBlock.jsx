3d01e1c3a4600e94f84ff3f98e6641cc98e61c03
Soye Usoroh <usorohsoye@gmail.com>
2025-04-22 19:30:43 +0100

    add css to QuoteBlock component

--- src/components/QuoteBlock.jsx
+++ src/components/QuoteBlock.jsx
@@ -1,9 +1,11 @@
+import "./styles/QuoteBlock.css";
+
 const QuoteBlock = () => {
   return (
-    <section>
-      <h5>‘‘Nothing is true and everything is forbidden’’</h5>
-      <p>Assassin’s Creed</p>
-      <p>2007</p>
+    <section className="quote-block">
+      <h5 className="quote">‘‘Nothing is true and everything is forbidden’’</h5>
+      <p className="narrator">Assassin’s Creed</p>
+      <p className="year">2007</p>
     </section>
   );
 };
