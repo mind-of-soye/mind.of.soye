58b0ec8b917aa4b41f9cd20bda1d548330784836
Soye Usoroh <usorohsoye@gmail.com>
2025-04-22 19:19:45 +0100

    add HTML to CodeBlock.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Main from "./components/Main";
 import Taskbar from "./components/Taskbar";
 import NavBar from "./components/NavBar";
+import QuoteBlock from "./components/QuoteBlock";
 
 function App() {
   return (
     <>
+      <Taskbar />
+
       <section>
-        <Taskbar />
         <Main />
+        <QuoteBlock />
       </section>
 
       <NavBar />
---
+++ src/components/QuoteBlock.jsx
@@ -0,0 +1,10 @@
+const QuoteBlock = () => {
+  return (
+    <section>
+      <h5>‘‘Nothing is true and everything is forbidden’’</h5>
+      <p>Assassin’s Creed</p>
+      <p>2007</p>
+    </section>
+  );
+};
+export default QuoteBlock;
