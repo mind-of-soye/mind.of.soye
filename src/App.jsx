6bd2010e1dea9325696eaa5099e0939d2834a7d7
Soye Usoroh <usorohsoye@gmail.com>
2025-04-22 18:13:27 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <div>
+      <section>
         <Taskbar />
         <Main />
-      </div>
+      </section>
 
       <NavBar />
     </>
