e982ff081d5bac089a131a7b0d0686f0d5612d82
Soye Usoroh <usorohsoye@gmail.com>
2025-04-22 14:54:46 +0100

    create codeblock and NavBar.jsx

--- src/components/Codeblock.jsx
+++ src/components/Codeblock.jsx
@@ -3,7 +3,10 @@ import "./styles/Codeblock.css";
 const Codeblock = () => {
   return (
     <div className="code-container">
-      <p className="code-text">{`{name = “Soye”, role = “Programmer”}`}</p>
+      <p className="code-text">
+        &#123;name = "Soye", role = "
+        <span className="code-highlight">Programmer</span>"&#125;
+      </p>
     </div>
   );
 };
